2386a5556aad3b9f6fbfea401129c74da5117764
谢武龙 <wulongxie@caih.com>
2020-08-11 10:00:24 -0400

    初始化优化

--- rails.sh
+++ rails.sh
@@ -6,13 +6,16 @@ CWD=`pwd`
 # Current script directory
 CSD=$(cd `dirname $0`; pwd)
 
+GID=`id | awk '{print $2}' | awk -F "(" '{print $1}' | awk -F "=" '{print $2}'`
+
 cd $CSD
 
 if [ -f "rails_new.sh" ]; then
   docker run --rm -v $CSD:/app xiewulong/rails:alpine sh -c /app/rails_new.sh
   rm -rf rails_new.sh
+  docker run --rm -v $CSD:/app xiewulong/rails:alpine sh -c "cd /app && chown $UID:$GID -R ."
+else
+  docker exec app sh -c "rails $@ && chown $USER:$GID -R ."
 fi
-
-docker run --rm -v $CSD:/app xiewulong/rails:alpine sh -c "cd /app && rails $@ && adduser -DH $USER && chown $USER:$USER -R ."
 
 cd $CWD
