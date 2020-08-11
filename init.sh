17bf8cb6e98776d0f5a93cad16c21dc1d624f603
谢武龙 <wulongxie@caih.com>
2020-08-11 01:05:59 -0400

    动态脚本路径判断

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ set -ex
 gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
 bundle config mirror.https://rubygems.org https://gems.ruby-china.com
 
-cd /app
+cd `dirname $0`
 
 rails new --api -B .
 bundle lock --add-platform x86-mingw32 x86-mswin32 x64-mingw32 java
