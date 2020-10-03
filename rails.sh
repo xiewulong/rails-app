554b1bf0395b701e24771308322191fe2b6e2fc3
谢武龙 <wulongxie@caih.com>
2020-10-02 23:18:54 -0400

    更新文档为统一doc输出

--- rails.sh
+++ rails.sh
@@ -14,12 +14,9 @@ if [ -f "rails_new.sh" ]; then
   docker run --rm -i -v $CSD:/app xiewulong/rails:alpine sh -c /app/rails_new.sh
   rm -rf rails_new.sh
   docker run --rm -i -v $CSD:/app xiewulong/rails:alpine sh -c "cd /app && chown $UID:$GID -R ."
-elif [ $1 = "apidoc" ]; then
-  docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i && npm run apidoc && chown $UID:$GID -R ./node_modules ./public/apidoc"
-  # docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i --registry=https://registry.npm.taobao.org && npm run apidoc && chown $UID:$GID -R ./node_modules ./public/apidoc"
-elif [ $1 = "swagger" ]; then
-  docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i && npm run swagger && chown $UID:$GID -R ./node_modules ./public/swagger"
-  # docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i --registry=https://registry.npm.taobao.org && npm run swagger && chown $UID:$GID -R ./node_modules ./public/swagger"
+elif [ $1 = "doc" ]; then
+  docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i && npm run doc && chown $UID:$GID -R ./node_modules ./public/doc"
+  # docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i --registry=https://registry.npm.taobao.org && npm run doc && chown $UID:$GID -R ./node_modules ./public/doc"
 else
   docker-compose exec app rails $@
   docker-compose exec app chown $UID:$GID -R .
