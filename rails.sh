1ad4d3edfe5f3c5b2259a00f8b0b6afd94b43d7e
谢武龙 <wulongxie@caih.com>
2020-10-11 12:28:46 -0400

    过滤node_modules和docker-compose.yml

--- rails.sh
+++ rails.sh
@@ -14,6 +14,10 @@ if [ -f "rails_new.sh" ]; then
   docker run --rm -i -v $CSD:/app xiewulong/rails:alpine sh -c /app/rails_new.sh
   rm -rf rails_new.sh
   docker run --rm -i -v $CSD:/app xiewulong/rails:alpine sh -c "cd /app && chown $UID:$GID -R ."
+  echo '' >> ./.gitignore
+  echo '# Custom' >> ./.gitignore
+  echo '/node_modules' >> ./.gitignore
+  echo 'docker-compose.yml' >> ./.gitignore
 elif [ "$1" = "doc" ]; then
   docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i && npm run doc && chown $UID:$GID -R ./node_modules ./public/doc"
   # docker run --rm -i -v $CSD:/app node:current-alpine sh -c "cd /app && npm i --registry=https://registry.npm.taobao.org && npm run doc && chown $UID:$GID -R ./node_modules ./public/doc"
